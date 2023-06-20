3835a31eaf0f986e933fe0b0b1a42e98d0b46051
Moch. Avin <5025221061@mhs.its.ac.id>
2023-06-20 08:00:35 +0700

    completed task at the bottom list and formatting code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-// Mendapatkan referensi ke elemen HTML
 var todoInput = document.getElementById('item');
 var addButton = document.getElementById('add');
 var todoList = document.getElementById('todo');
 
-// Mendefinisikan array untuk menyimpan daftar to-do
 var todos = [];
 
-// Menambahkan event listener untuk tombol "Add"
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', function () {
   var todoText = todoInput.value;
   if (todoText.trim() !== '') {
     addTodoToList(todoText);
@@ -16,7 +13,7 @@ addButton.addEventListener('click', function() {
 });
 
 // Menambahkan event listener untuk input field
-todoInput.addEventListener('keydown', function(e) {
+todoInput.addEventListener('keydown', function (e) {
   var todoText = todoInput.value;
   if (e.code === 'Enter' && todoText.trim() !== '') {
     addTodoToList(todoText);
@@ -28,9 +25,9 @@ todoInput.addEventListener('keydown', function(e) {
 function addTodoToList(todoText) {
   var todoItem = {
     text: todoText,
-    completed: false
+    completed: false,
   };
-  todos.push(todoItem);
+  todos.unshift(todoItem);
   renderTodoList();
 }
 
@@ -43,6 +40,15 @@ function removeTodoFromList(index) {
 // Fungsi untuk menandai to-do sebagai selesai atau belum selesai
 function toggleTodoCompletion(index) {
   todos[index].completed = !todos[index].completed;
+  if (todos[index].completed) {
+    todos.push(todos[index]);
+    todos.splice(index, 1);
+  }else{
+    let temp = todos[index];
+    todos.splice(index, 1);
+    todos.unshift(temp);
+    
+  }
   renderTodoList();
 }
 
@@ -50,40 +56,72 @@ function toggleTodoCompletion(index) {
 function renderTodoList() {
   todoList.innerHTML = '';
 
-  todos.forEach(function(todoItem, index) {
+  todos.forEach(function (todoItem, index) {
     var todoItemElement = document.createElement('li');
-    todoItemElement.classList.add("bg-purple-100", "font-[450]",  "w-full", "flex", "justify-between", "px-4", "py-4", "border-b-8", "border-purple-200", "hover:bg-purple-300", "rounded-md", "transition", "duration-150", "hover:p-5");
+    todoItemElement.classList.add(
+      'bg-purple-100',
+      'font-[450]',
+      'w-full',
+      'flex',
+      'justify-between',
+      'px-4',
+      'py-4',
+      'border-b-8',
+      'border-purple-200',
+      'hover:bg-purple-300',
+      'rounded-md',
+      'transition',
+      'duration-250',
+      'hover:p-6'
+    );
 
     var buttons = document.createElement('div');
-    buttons.classList.add("flex", "space-x-4");
+    buttons.classList.add('flex', 'space-x-4');
 
     var todoCompleteButton = document.createElement('button');
     todoCompleteButton.textContent = 'Complete';
-    todoCompleteButton.addEventListener('click', function() {
+    todoCompleteButton.addEventListener('click', function () {
       toggleTodoCompletion(index);
     });
 
-    todoCompleteButton.classList.add("bg-purple-500", "text-white", "px-4", "py-2", "rounded-md", "hover:bg-purple-600", "transition", "duration-150");
+    todoCompleteButton.classList.add(
+      'bg-purple-500',
+      'text-white',
+      'px-4',
+      'py-2',
+      'rounded-md',
+      'hover:bg-purple-600',
+      'transition',
+      'duration-150'
+    );
 
     var todoText = document.createElement('div');
     todoText.textContent = todoItem.text;
     if (todoItem.completed) {
       todoText.style.textDecoration = 'line-through';
     }
-    todoText.classList.add("font-[450]", "text-lg");
+    todoText.classList.add('font-[450]', 'text-lg');
 
     var todoRemoveButton = document.createElement('button');
     todoRemoveButton.textContent = 'Remove';
-    todoRemoveButton.addEventListener('click', function() {
+    todoRemoveButton.addEventListener('click', function () {
       removeTodoFromList(index);
     });
-    todoRemoveButton.classList.add("bg-purple-500", "text-white", "px-4", "py-2", "rounded-md", "hover:bg-purple-600", "transition", "duration-150");
+    todoRemoveButton.classList.add(
+      'bg-purple-500',
+      'text-white',
+      'px-4',
+      'py-2',
+      'rounded-md',
+      'hover:bg-purple-600',
+      'transition',
+      'duration-150'
+    );
 
     buttons.appendChild(todoCompleteButton);
     buttons.appendChild(todoRemoveButton);
     todoItemElement.appendChild(todoText);
     todoItemElement.appendChild(buttons);
-    
 
     todoList.appendChild(todoItemElement);
   });
